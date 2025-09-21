61dab1156e65d8fbf2eb9f034e3f65262c7fac25
MilkBlock <mineralsteins@gmail.com>
2025-09-21 21:35:22 +0800

    move ordered_hash_map into main crate

--- src/layout/nesting_graph.rs
+++ src/layout/nesting_graph.rs
@@ -1,7 +1,7 @@
-use graphlib_rust::Graph;
+use crate::layout::{util, GraphConfig, GraphEdge, GraphNode};
+use crate::ordered_hashmap::OrderedHashMap;
 use graphlib_rust::graph::GRAPH_NODE;
-use ordered_hashmap::OrderedHashMap;
-use crate::layout::{GraphConfig, GraphEdge, GraphNode, util};
+use graphlib_rust::Graph;
 
 /*
  * A nesting graph creates dummy nodes for the tops and bottoms of subgraphs,
@@ -27,171 +27,187 @@ use crate::layout::{GraphConfig, GraphEdge, GraphNode, util};
  * Graphs."
  */
 pub fn run(graph: &mut Graph<GraphConfig, GraphNode, GraphEdge>) {
-  let graph_node = GraphNode::default();
-  let root = util::add_dummy_node(graph, "root".to_string(), graph_node, "_root".to_string());
-  let depths = tree_depths(graph);
-  let mut height: usize = 0;
-  for depth in depths.values() {
-    if depth > &height {
-      height = depth.to_owned();
+    let graph_node = GraphNode::default();
+    let root = util::add_dummy_node(graph, "root".to_string(), graph_node, "_root".to_string());
+    let depths = tree_depths(graph);
+    let mut height: usize = 0;
+    for depth in depths.values() {
+        if depth > &height {
+            height = depth.to_owned();
+        }
     }
-  }
-  if height > 0 {
-    height -= 1;
-  }
-
-  let node_sep = (2 * height + 1) as f32;
-  graph.graph_mut().nesting_root = Some(root.clone());
-
-  // Multiply minlen by nodeSep to align nodes on non-border ranks.
-  let edge_objs = graph.edges();
-  for edge_obj in edge_objs.into_iter() {
-    let _edge_label = graph.edge_mut_with_obj(&edge_obj);
-    if _edge_label.is_none() {
-      continue;
+    if height > 0 {
+        height -= 1;
     }
-    let edge_label = _edge_label.unwrap();
-    edge_label.minlen = Some(edge_label.minlen.unwrap_or(1.0) * node_sep);
-  }
 
-  // Calculate a weight that is sufficient to keep subgraphs vertically compact
-  let weight = sum_weights(graph) + 1.0;
+    let node_sep = (2 * height + 1) as f32;
+    graph.graph_mut().nesting_root = Some(root.clone());
 
-  // Create border nodes and link them up
-  let children = graph.children(&GRAPH_NODE.to_string());
-  for child_id in children.into_iter() {
-    dfs(graph, &root, &node_sep, &weight, &height, &depths, &child_id);
-  }
+    // Multiply minlen by nodeSep to align nodes on non-border ranks.
+    let edge_objs = graph.edges();
+    for edge_obj in edge_objs.into_iter() {
+        let _edge_label = graph.edge_mut_with_obj(&edge_obj);
+        if _edge_label.is_none() {
+            continue;
+        }
+        let edge_label = _edge_label.unwrap();
+        edge_label.minlen = Some(edge_label.minlen.unwrap_or(1.0) * node_sep);
+    }
 
-  // Save the multiplier for node layers for later removal of empty border
-  // layers.
-  graph.graph_mut().node_rank_factor = Some(node_sep);
+    // Calculate a weight that is sufficient to keep subgraphs vertically compact
+    let weight = sum_weights(graph) + 1.0;
+
+    // Create border nodes and link them up
+    let children = graph.children(&GRAPH_NODE.to_string());
+    for child_id in children.into_iter() {
+        dfs(
+            graph, &root, &node_sep, &weight, &height, &depths, &child_id,
+        );
+    }
+
+    // Save the multiplier for node layers for later removal of empty border
+    // layers.
+    graph.graph_mut().node_rank_factor = Some(node_sep);
 }
 
 fn tree_depths(graph: &Graph<GraphConfig, GraphNode, GraphEdge>) -> OrderedHashMap<String, usize> {
-  let mut depths: OrderedHashMap<String, usize> = OrderedHashMap::new();
+    let mut depths: OrderedHashMap<String, usize> = OrderedHashMap::new();
 
-  fn dfs(node_id: String, depth: usize, depths: &mut OrderedHashMap<String, usize>, graph: &Graph<GraphConfig, GraphNode, GraphEdge>) {
-    let children = graph.children(&node_id);
-    for child_id in children.iter() {
-      // recursion for child node ids
-      dfs(child_id.clone(), depth + 1, depths, graph);
+    fn dfs(
+        node_id: String,
+        depth: usize,
+        depths: &mut OrderedHashMap<String, usize>,
+        graph: &Graph<GraphConfig, GraphNode, GraphEdge>,
+    ) {
+        let children = graph.children(&node_id);
+        for child_id in children.iter() {
+            // recursion for child node ids
+            dfs(child_id.clone(), depth + 1, depths, graph);
+        }
+        // setting for current node
+        depths.insert(node_id.clone(), depth);
     }
-    // setting for current node
-    depths.insert(node_id.clone(), depth);
-  }
 
-  // processing root nodes
-  for node_id in graph.children(&GRAPH_NODE.to_string()) {
-    dfs(node_id, 1, &mut depths, graph);
-  }
-  return depths;
+    // processing root nodes
+    for node_id in graph.children(&GRAPH_NODE.to_string()) {
+        dfs(node_id, 1, &mut depths, graph);
+    }
+    return depths;
 }
 
-fn dfs(graph: &mut Graph<GraphConfig, GraphNode, GraphEdge>, root: &String, node_sep: &f32, weight: &f32, height: &usize, depths: &OrderedHashMap<String, usize>, node_id: &String) {
-  let children = graph.children(node_id);
-  if children.len() == 0 {
-    if node_id != root {
-      let mut graph_edge = GraphEdge::default();
-      graph_edge.minlen = Some(node_sep.clone());
-      graph_edge.weight = Some(0.0);
-      let _ = graph.set_edge(&root, &node_id, Some(graph_edge), None);
-    }
-    return ();
-  }
-
-  let top = util::add_border_node(graph, "_bt", None, None);
-  let bottom = util::add_border_node(graph, "_bb", None, None);
-
-  let _pt = graph.set_parent(&top, Some(node_id.clone()));
-  let _pb = graph.set_parent(&bottom, Some(node_id.clone()));
-
-  let _label = graph.node_mut(node_id);
-  if let Some(label) = _label {
-    label.border_top = Some(top.clone());
-    label.border_bottom = Some(bottom.clone());
-  }
-
-  for child_id in children.into_iter() {
-    dfs(graph, root, node_sep, weight, height, depths, &child_id);
-
-    let _child_node = graph.node(&child_id);
-    if _child_node.is_none() {
-      continue;
+fn dfs(
+    graph: &mut Graph<GraphConfig, GraphNode, GraphEdge>,
+    root: &String,
+    node_sep: &f32,
+    weight: &f32,
+    height: &usize,
+    depths: &OrderedHashMap<String, usize>,
+    node_id: &String,
+) {
+    let children = graph.children(node_id);
+    if children.len() == 0 {
+        if node_id != root {
+            let mut graph_edge = GraphEdge::default();
+            graph_edge.minlen = Some(node_sep.clone());
+            graph_edge.weight = Some(0.0);
+            let _ = graph.set_edge(&root, &node_id, Some(graph_edge), None);
+        }
+        return ();
     }
 
-    let child_node = _child_node.unwrap();
-    let border_top = child_node.border_top.clone();
-    let border_bottom = child_node.border_bottom.clone();
+    let top = util::add_border_node(graph, "_bt", None, None);
+    let bottom = util::add_border_node(graph, "_bb", None, None);
 
-    let mut child_top = child_id.clone();
-    let mut child_bottom = child_id.clone();
-    let mut this_weight: f32 = weight.clone();
-    let mut minlen: usize = 1;
+    let _pt = graph.set_parent(&top, Some(node_id.clone()));
+    let _pb = graph.set_parent(&bottom, Some(node_id.clone()));
 
-    if border_top.is_some() {
-      child_top = border_top.clone().unwrap();
-    }
-    if border_bottom.is_some() {
-      child_bottom = border_bottom.unwrap();
-    }
-    if border_top.is_some() {
-      this_weight = 2.0 * weight.clone();
-    }
-    if child_top == child_bottom {
-      minlen = height - depths.get(node_id).cloned().unwrap_or(0) + 1;
+    let _label = graph.node_mut(node_id);
+    if let Some(label) = _label {
+        label.border_top = Some(top.clone());
+        label.border_bottom = Some(bottom.clone());
     }
 
-    let mut _ct_graph_edge = GraphEdge::default();
-    _ct_graph_edge.minlen = Some(minlen.clone() as f32);
-    _ct_graph_edge.weight = Some(this_weight.clone());
-    _ct_graph_edge.nesting_edge = Some(true);
-    let _ct = graph.set_edge(&top, &child_top, Some(_ct_graph_edge), None);
+    for child_id in children.into_iter() {
+        dfs(graph, root, node_sep, weight, height, depths, &child_id);
 
-    let mut _cb_graph_edge = GraphEdge::default();
-    _cb_graph_edge.minlen = Some(minlen.clone() as f32);
-    _cb_graph_edge.weight = Some(this_weight.clone());
-    _cb_graph_edge.nesting_edge = Some(true);
-    let _cb = graph.set_edge(&child_bottom, &bottom, Some(_cb_graph_edge), None);
-  }
+        let _child_node = graph.node(&child_id);
+        if _child_node.is_none() {
+            continue;
+        }
 
-  if graph.parent(node_id).is_none() {
-    let mut graph_edge = GraphEdge::default();
-    graph_edge.minlen = Some((depths.get(node_id).cloned().unwrap_or(0) + height.clone()) as f32);
-    graph_edge.weight = Some(0.0);
-    graph_edge.nesting_edge = Some(true);
-    let _ = graph.set_edge(&root, &top, Some(graph_edge), None);
-  }
+        let child_node = _child_node.unwrap();
+        let border_top = child_node.border_top.clone();
+        let border_bottom = child_node.border_bottom.clone();
+
+        let mut child_top = child_id.clone();
+        let mut child_bottom = child_id.clone();
+        let mut this_weight: f32 = weight.clone();
+        let mut minlen: usize = 1;
+
+        if border_top.is_some() {
+            child_top = border_top.clone().unwrap();
+        }
+        if border_bottom.is_some() {
+            child_bottom = border_bottom.unwrap();
+        }
+        if border_top.is_some() {
+            this_weight = 2.0 * weight.clone();
+        }
+        if child_top == child_bottom {
+            minlen = height - depths.get(node_id).cloned().unwrap_or(0) + 1;
+        }
+
+        let mut _ct_graph_edge = GraphEdge::default();
+        _ct_graph_edge.minlen = Some(minlen.clone() as f32);
+        _ct_graph_edge.weight = Some(this_weight.clone());
+        _ct_graph_edge.nesting_edge = Some(true);
+        let _ct = graph.set_edge(&top, &child_top, Some(_ct_graph_edge), None);
+
+        let mut _cb_graph_edge = GraphEdge::default();
+        _cb_graph_edge.minlen = Some(minlen.clone() as f32);
+        _cb_graph_edge.weight = Some(this_weight.clone());
+        _cb_graph_edge.nesting_edge = Some(true);
+        let _cb = graph.set_edge(&child_bottom, &bottom, Some(_cb_graph_edge), None);
+    }
+
+    if graph.parent(node_id).is_none() {
+        let mut graph_edge = GraphEdge::default();
+        graph_edge.minlen =
+            Some((depths.get(node_id).cloned().unwrap_or(0) + height.clone()) as f32);
+        graph_edge.weight = Some(0.0);
+        graph_edge.nesting_edge = Some(true);
+        let _ = graph.set_edge(&root, &top, Some(graph_edge), None);
+    }
 }
 
 fn sum_weights(graph: &Graph<GraphConfig, GraphNode, GraphEdge>) -> f32 {
-  let mut total_weights: f32 = 0.0;
+    let mut total_weights: f32 = 0.0;
 
-  for edge in graph.edges() {
-    if let Some(edge_label) = graph.edge_with_obj(&edge) {
-      if let Some(weight) = edge_label.weight {
-        total_weights += weight;
-      }
+    for edge in graph.edges() {
+        if let Some(edge_label) = graph.edge_with_obj(&edge) {
+            if let Some(weight) = edge_label.weight {
+                total_weights += weight;
+            }
+        }
     }
-  }
 
-  return total_weights;
+    return total_weights;
 }
 
 pub fn cleanup(graph: &mut Graph<GraphConfig, GraphNode, GraphEdge>) {
-  let graph_label = graph.graph();
-  if graph_label.nesting_root.is_some() {
-    graph.remove_node(&graph_label.nesting_root.clone().unwrap());
-  }
-  graph.graph_mut().nesting_root = None;
-  // removing nesting edge
-  let edges = graph.edges();
-  for edge in edges.into_iter() {
-    let _edge_label = graph.edge_with_obj(&edge);
-    if let Some(edge_label) = _edge_label {
-      if edge_label.nesting_edge.clone().unwrap_or(false) {
-        graph.remove_edge_with_obj(&edge);
-      }
+    let graph_label = graph.graph();
+    if graph_label.nesting_root.is_some() {
+        graph.remove_node(&graph_label.nesting_root.clone().unwrap());
     }
-  }
+    graph.graph_mut().nesting_root = None;
+    // removing nesting edge
+    let edges = graph.edges();
+    for edge in edges.into_iter() {
+        let _edge_label = graph.edge_with_obj(&edge);
+        if let Some(edge_label) = _edge_label {
+            if edge_label.nesting_edge.clone().unwrap_or(false) {
+                graph.remove_edge_with_obj(&edge);
+            }
+        }
+    }
 }
--- src/layout/rank/util.rs
+++ src/layout/rank/util.rs
@@ -1,6 +1,6 @@
-use graphlib_rust::{Edge, Graph};
-use ordered_hashmap::OrderedHashMap;
+use crate::ordered_hashmap::OrderedHashMap;
 use crate::{GraphConfig, GraphEdge, GraphNode};
+use graphlib_rust::{Edge, Graph};
 
 /*
  * Initializes ranks for the input graph using the longest path algorithm. This
@@ -25,31 +25,50 @@ use crate::{GraphConfig, GraphEdge, GraphNode};
  */
 
 pub fn longest_path(g: &mut Graph<GraphConfig, GraphNode, GraphEdge>) {
-  let mut visited: OrderedHashMap<String, bool> = OrderedHashMap::new();
+    let mut visited: OrderedHashMap<String, bool> = OrderedHashMap::new();
 
-  fn dfs(v: &String, g: &mut Graph<GraphConfig, GraphNode, GraphEdge>, visited: &mut OrderedHashMap<String, bool>) -> i32 {
-    let node_label = g.node(v);
-    if visited.contains_key(v) {
-      return node_label.cloned().unwrap_or(GraphNode::default()).rank.unwrap_or(0);
+    fn dfs(
+        v: &String,
+        g: &mut Graph<GraphConfig, GraphNode, GraphEdge>,
+        visited: &mut OrderedHashMap<String, bool>,
+    ) -> i32 {
+        let node_label = g.node(v);
+        if visited.contains_key(v) {
+            return node_label
+                .cloned()
+                .unwrap_or(GraphNode::default())
+                .rank
+                .unwrap_or(0);
+        }
+        visited.insert(v.clone(), true);
+
+        let ranks: Vec<i32> = g
+            .out_edges(v, None)
+            .unwrap_or(vec![])
+            .iter()
+            .map(|e| {
+                dfs(&e.w, g, visited)
+                    - (g.edge_with_obj(&e)
+                        .cloned()
+                        .unwrap_or(GraphEdge::default())
+                        .minlen
+                        .unwrap_or(0.0)
+                        .round() as i32)
+            })
+            .collect();
+        let rank: i32 = ranks.iter().min().cloned().unwrap_or(0) as i32;
+        {
+            let _node_label = g.node_mut(v);
+            if let Some(node_label) = _node_label {
+                node_label.rank = Some(rank.clone());
+            }
+        }
+        return rank;
     }
-    visited.insert(v.clone(), true);
 
-    let ranks: Vec<i32> = g.out_edges(v, None).unwrap_or(vec![]).iter().map(|e| {
-      dfs(&e.w, g, visited) - (g.edge_with_obj(&e).cloned().unwrap_or(GraphEdge::default()).minlen.unwrap_or(0.0).round() as i32)
-    }).collect();
-    let rank: i32 = ranks.iter().min().cloned().unwrap_or(0) as i32;
-    {
-      let _node_label = g.node_mut(v);
-      if let Some(node_label) = _node_label {
-        node_label.rank = Some(rank.clone());
-      }
+    for node_id in g.sources().into_iter() {
+        dfs(&node_id, g, &mut visited);
     }
-    return rank;
-  }
-
-  for node_id in g.sources().into_iter() {
-    dfs(&node_id, g, &mut visited);
-  }
 }
 
 /*
@@ -57,8 +76,24 @@ pub fn longest_path(g: &mut Graph<GraphConfig, GraphNode, GraphEdge>) {
  * difference between the length of the edge and its minimum length.
  */
 pub fn slack(g: &Graph<GraphConfig, GraphNode, GraphEdge>, e: &Edge) -> i32 {
-  let w_rank = g.node(&e.w).cloned().unwrap_or(GraphNode::default()).rank.unwrap_or(0);
-  let v_rank = g.node(&e.v).cloned().unwrap_or(GraphNode::default()).rank.unwrap_or(0);
-  let minlen = g.edge_with_obj(e).cloned().unwrap_or(GraphEdge::default()).minlen.unwrap_or(10.0).round() as i32;
-  return w_rank - v_rank - minlen;
+    let w_rank = g
+        .node(&e.w)
+        .cloned()
+        .unwrap_or(GraphNode::default())
+        .rank
+        .unwrap_or(0);
+    let v_rank = g
+        .node(&e.v)
+        .cloned()
+        .unwrap_or(GraphNode::default())
+        .rank
+        .unwrap_or(0);
+    let minlen = g
+        .edge_with_obj(e)
+        .cloned()
+        .unwrap_or(GraphEdge::default())
+        .minlen
+        .unwrap_or(10.0)
+        .round() as i32;
+    return w_rank - v_rank - minlen;
 }
